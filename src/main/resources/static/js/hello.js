910d2ced7483967a4f92eb8161b14d6a22f68de0
Ben J Shalom <Ben.J@Altisource.com>
2016-04-22 16:52:27 +0530

    Implemented test log tail

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -6,3 +6,25 @@ $(document).ready(function() {
        $('.greeting-content').append(data.content);
     });
 });
+
+$("#tabs-3").load(function() {
+	console.log('Tab - focused');
+	$.ajax({
+		url: "http://localhost:8080/monitor?server=capture-dev1-wrangler1.altidev.net&service=Bootstrap"
+	}).then(function(data) {
+		$("#tabs-3").children("p").append(data);
+	});
+});
+
+$( "#tabs" ).tabs({
+	activate: function( event, ui ) {
+		console.log('tab activated');
+		if(ui.newTab.index() == $("#tabs-3")) {
+			$.ajax({
+				url: "http://localhost:8080/monitor?server=capture-dev1-wrangler1.altidev.net&service=Bootstrap"
+			}).then(function(data) {
+				$("#tabs-3").children("p").append(data);
+			});
+        }
+	}
+	});
